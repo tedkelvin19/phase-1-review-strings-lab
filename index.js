3e557746a6938734cd25674ba9faa4aff0bad331
TedKelvin <tedkelvin19@gmail.com>
2023-09-18 14:40:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Kelvin"
+let welcomeMessage ="Welcome to Flatbook, "  + currentUser + "!"  
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() +" !"
+let shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!"
